7ddb106443918af9a0b97801d5288c3d37658341
Chuck Wood <chuck@pmamediagroup.com>
2009-06-16 09:21:36 -0600

    authentication metal

--- app/metal/authentication.rb
+++ app/metal/authentication.rb
@@ -4,11 +4,12 @@ require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Ra
 class Authentication
   def self.call(env)
     session = env['rack.session']
-    puts session.inspect
-    if Path.user_authorized?(env["PATH_INFO"], session[:user_id])
-      [404, {"Content-Type" => "text/html"}, ["Not Found"]]
-    else
-      [302, {"Content-Type" => "text/html", "Location" => "/login"}, ["You must log in."]]
+    if env["PATH_INFO"] =~ /\/admin.*/
+      if session[:user_id]
+        [404, {"Content-Type" => "text/html"}, ["Not Found"]]
+      else
+        [302, {"Content-Type" => "text/html", "Location" => "/login"}, ["You must log in."]]
+      end
     end
   end
 end
